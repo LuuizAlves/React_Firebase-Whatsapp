d45fb8d398773f1d45ab980f6888197224ba79fa
Luiz Felipe <luizfelipeduartealves@outlook.com>
2020-09-01 23:36:58 -0400

    Making Sidebar - header and search

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
+
 import './Sidebar.css';
 
 function Sidebar(){
-    return(
-        <div className="sidebar">
-            <h1 className="title">Sidebar</h1>
-        </div>
-    )
+   return(
+      <div className="sidebar">
+         <div className="sidebar_header">
+            <Avatar />
+      
+            <div className="sidebar_headerRight">
+               <IconButton>
+                  <DonutLargeIcon />
+               </IconButton>
+
+               <IconButton>
+                  <ChatIcon />
+               </IconButton>
+
+               <IconButton>
+                  <MoreVertIcon />
+               </IconButton>
+            </div>
+         </div>
+
+         <div className="sidebar_search">
+            <div className="sidebar_searchContainer">
+               <SearchOutlined />
+
+               <input type="text" placeholder="Procure aqui"/>
+            </div>
+         </div>
+
+         <div className="sidebar_chats">
+
+         </div>
+      </div>
+   )
 }
 
 export default Sidebar;
