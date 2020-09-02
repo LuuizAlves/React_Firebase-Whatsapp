7a02a7493a673187ffc947b31e14ae9007cf2d46
Luiz Felipe <luizfelipeduartealves@outlook.com>
2020-09-01 22:25:04 -0400

    Create project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Sidebar from './components/Sidebar/Sidebar';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="app_body">
+                <Sidebar />
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8lIEIiCDeeVxivqPKuYUP7zMxNwZ_Z1k",
+    authDomain: "whatsapp---clone-61473.firebaseapp.com",
+    databaseURL: "https://whatsapp---clone-61473.firebaseio.com",
+    projectId: "whatsapp---clone-61473",
+    storageBucket: "whatsapp---clone-61473.appspot.com",
+    messagingSenderId: "96422048440",
+    appId: "1:96422048440:web:64c7342b9875f408cd3a4d",
+    measurementId: "G-1EM82C4X1C"
+};
